6a01cab931ff7b5086c6488e51d743a474e33bb2
Cristian Castano <cristiancastano852@gmail.com>
2024-09-15 23:30:48 -0500

    test: Add test for invoice payment exception handling

--- tests/Feature/Invoice/InvoicesPaymentTest.php
+++ tests/Feature/Invoice/InvoicesPaymentTest.php
@@ -32,4 +32,28 @@ class InvoicesPaymentTest extends TestCase
         $response = $this->post(route('invoice.invoicesPayment', $microsite), ['invoice_id' => $invoice_id]);
         $response->assertStatus(302);
     }
+
+    public function test_invoce_payment_exception(): void
+    {
+        Http::fake([
+            'https://checkout-co.placetopay.dev/api/session' => Http::response([
+                'status' => [
+                    'status' => 'exception',
+                    'reason' => 'PC',
+                    'message' => 'Session created successfully',
+                    'date' => '2021-09-29T16:00:00-05:00',
+                ],
+                'requestId' => '123',
+                'processUrl' => 'https://checkout-co.placetopay.dev/session/123456',
+            ]),
+        ]);
+        $user = User::factory()->create();
+        $microsite = Microsites::factory()->create();
+        $this->actingAs($user);
+        $invoice = Invoice::factory()->create(['microsite_id' => $microsite->id]);
+        $invoice_id = $invoice->id;
+        $response = $this->post(route('invoice.invoicesPayment', $microsite), ['invoice_id' => $invoice_id]);
+        $response->assertStatus(302);
+
+    }
 }
