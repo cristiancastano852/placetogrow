7caac8cab205aefb58feaf0ae5a5212f7fee94c9
Cristian Castano <cristiancastano852@gmail.com>
2024-09-15 22:02:24 -0500

    feat: improve code

--- app/Services/Payments/SubscriptionService.php
+++ app/Services/Payments/SubscriptionService.php
@@ -122,14 +122,14 @@ class SubscriptionService
         return $response;
     }
 
-    public function cancel(): ClientResponse | null
+    public function cancel(): ?ClientResponse
     {
         $response = null;
         if ($this->subscription->token !== null and $this->subscription->subtoken !== null) {
             $response = $this->gateway->prepare()
                 ->invalidateTokenSubtoken($this->subscription->token, $this->subscription->subtoken);
         }
-        
+
         $this->subscription->update([
             'status' => SubscriptionStatus::CANCELED->value,
         ]);
